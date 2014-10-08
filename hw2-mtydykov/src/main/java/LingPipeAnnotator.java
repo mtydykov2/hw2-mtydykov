eb649baff16ebb2ba7d8b0000093393cb961af3f
Maya Tydykov <mtydykov@andrew.cmu.edu>
2014-10-07 22:59:00 -0400

    fixed model path

--- hw2-mtydykov/src/main/java/LingPipeAnnotator.java
+++ hw2-mtydykov/src/main/java/LingPipeAnnotator.java
@@ -30,41 +30,38 @@ public class LingPipeAnnotator extends JCasAnnotator_ImplBase {
 
   private static final String LINGPIPE_ID = "0";
 
-  private File modelFile = null;
-
   private final static String PARAM_MODEL_FILE = "ModelFileName";
+
   private Chunker chunker = null;
+
   @Override
   public void process(JCas arg0) throws AnalysisEngineProcessException {
 
     FSIterator it = arg0.getAnnotationIndex(SentenceData.type).iterator();
-    if (modelFile != null) {
-      while (it.hasNext()) {
-        SentenceData curr = (SentenceData) it.next();
-        Chunking chunkIterator = chunker.chunk(curr.getSentText().toCharArray(), 0,
-                curr.getSentText().toCharArray().length);
-        for(Chunk c: chunkIterator.chunkSet()){
-          GeneMention mention = new GeneMention(arg0);
-          mention.setSentenceId(curr.getSentId());
-          mention.setBeginAndEnd(c.start(), c.end(), curr.getSentText());
-          mention.setMentionText(curr.getSentText().substring(c.start(),
-                  c.end()));
-          mention.setSentenceId(curr.getSentId());
-          mention.setCasProcessorId(LINGPIPE_ID);
-          mention.addToIndexes();
-        }
+    while (it.hasNext()) {
+      SentenceData curr = (SentenceData) it.next();
+      Chunking chunkIterator = chunker.chunk(curr.getSentText().toCharArray(), 0, curr
+              .getSentText().toCharArray().length);
+      for (Chunk c : chunkIterator.chunkSet()) {
+        GeneMention mention = new GeneMention(arg0);
+        mention.setSentenceId(curr.getSentId());
+        mention.setBeginAndEnd(c.start(), c.end(), curr.getSentText());
+        mention.setMentionText(curr.getSentText().substring(c.start(), c.end()));
+        mention.setSentenceId(curr.getSentId());
+        mention.setCasProcessorId(LINGPIPE_ID);
+        mention.addToIndexes();
       }
     }
+
   }
 
   /**
    * Load model file.
    */
   public void initialize(UimaContext aContext) {
-    this.modelFile = new File((String) aContext.getConfigParameterValue(PARAM_MODEL_FILE));
     try {
-      this.chunker = (Chunker) AbstractExternalizable
-              .readObject(modelFile);
+      this.chunker = (Chunker) AbstractExternalizable.readResourceObject((String) aContext
+              .getConfigParameterValue("PARAM_MODEL_FILE"));
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
